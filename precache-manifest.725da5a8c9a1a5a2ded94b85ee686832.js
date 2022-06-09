7ae4e2cc04ab5a02b647edbe0421f4932dd2ae46
David Agbaeze <you@example.com>
2022-06-09 05:06:21 +0100

    Updates

---
+++ precache-manifest.725da5a8c9a1a5a2ded94b85ee686832.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f7cb78bd2ee4a653e96",
+    "url": "/robofriends/static/css/main.4657dc40.chunk.css"
+  },
+  {
+    "revision": "7f7cb78bd2ee4a653e96",
+    "url": "/robofriends/static/js/main.7f7cb78b.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/robofriends/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "c71514a05d48e7f46d0c",
+    "url": "/robofriends/static/css/2.48378149.chunk.css"
+  },
+  {
+    "revision": "c71514a05d48e7f46d0c",
+    "url": "/robofriends/static/js/2.c71514a0.chunk.js"
+  },
+  {
+    "revision": "5a6fc926011dbcb5a003",
+    "url": "/robofriends/static/js/runtime~main.5a6fc926.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "13fde46cf11c5aee3f4f20d89ca50697",
+    "url": "/robofriends/index.html"
+  }
+];
